0c223030ec9d181e54a0e85bca25457288466e2c
MarcOS-X <simon.marc.olivier@gmail.com>
2020-03-27 15:32:02 -0400

    fix6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
